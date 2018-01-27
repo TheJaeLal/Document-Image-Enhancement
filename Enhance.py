ee879a6f4fd3b360f8697f4c2f157a8ff6f8e0ab
thejaelal <jay.lal@somaiya.edu>
2018-01-27 16:18:24 +0530

    defined w,b and converted to grayscale

--- Enhance.py
+++ Enhance.py
@@ -1,3 +1,15 @@
 import cv2
+import numpy as np
 
 src = cv2.imread('Images/src.jpg')
+
+src = cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
+
+height,weight = src.shape
+
+b = np.zeros((height,weight))
+w = np.ones((height,weight))
+
+dest = w*src + b
+
+cv2.imwrite('Output.jpg',dest)
