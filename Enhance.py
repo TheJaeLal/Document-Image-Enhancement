b029a56d23fbfb14cfa3d2d43e2611b93ed583ae
thejaelal <jay.lal@somaiya.edu>
2018-01-27 17:48:45 +0530

    Implemented all methods, getting non-satisfactory output

--- Enhance.py
+++ Enhance.py
@@ -1,19 +1,33 @@
 #HyperPrameters
-epochs,k1,k2 = (100,1,1)
+epochs,alpha,k1,k2 = (20,0.01,1,2)
 
 import cv2
 import numpy as np
 
 
 def find_gradient(I):
-	return
+	kernel_x = np.array([[-1,-1,-1],[0,0,0],[1,1,1]])
+	kernel_y = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+	grad_x = cv2.filter2D(I,-1,kernel_x)
+	grad_y = cv2.filter2D(I,-1,kernel_y)
+
+	return np.array([grad_x,grad_y])
 
 def calculate_delta(I,J,k1,k2):
 
 	grad_I = find_gradient(I)
 	grad_J = find_gradient(J)
 	
-	dw = 2*k1*I*(J-255) + 2*grad_I*(grad_J - grad_I)
+	result = grad_I*(grad_J - grad_I)
+	result_x = result[0]
+	result_y = result[1]
+
+	new_result = result_x+result_y
+	
+	print("Shape of new_result = {}".format(new_result.shape))
+
+
+	dw = 2*k1*I*(J-255) + k2*2*new_result
 	db = 2*k1*(J-255)
 
 	return dw,db
@@ -35,8 +49,10 @@ w = np.ones((height,weight))
 J = np.zeros((height,weight))
 
 for e in range(epochs):
-	J = w*I + b
+	print("Epoch {}".format(e))
 	dw,db = calculate_delta(I,J,k1,k2)
-	w,b = update_weights(w,dw,b,db,alpha=0.5)
+	w,b = update_weights(w,dw,b,db,alpha)
+	J = w*I + b
+	cv2.imwrite("Outputs/"+str(e)+".jpg",J)
 
-cv2.imwrite('Output.jpg',J)
+#cv2.imwrite('Output.jpg',J)
